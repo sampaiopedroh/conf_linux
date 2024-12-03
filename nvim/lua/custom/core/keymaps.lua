ac40d7b2dd155a17805e98196e8d36d84b7b0644
Pedro Sampaio <98555328+sampaiopedroh@users.noreply.github.com>
2024-12-03 19:32:04 -0300

    Add files via upload

---
+++ nvim/lua/custom/core/keymaps.lua
@@ -0,0 +1,2 @@
+--vim.keymap.set('n', '<C-Up>', ':m .-2<CR>==gi', { noremap = true, desc = 'Move line up' })
+--vim.keymap.set('n', '<C-Down>', ':m .+1<CR>==gi', { noremap = true, desc = 'Move line down' })
